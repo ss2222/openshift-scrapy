b374c06aa788b8c8cfce8a1f9f5bfae8622126ba
soheil <ss@ferdowsi-elec.tk>
2015-01-22 00:56:48 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,28 @@
 import os
 
 if __name__ == '__main__':
-
-  data_dir = os.environ['OPENSHIFT_DATA_DIR']
-   
-  config_values = {
-    'bind_address': os.environ['OPENSHIFT_PYTHON_IP'],
-    'http_port': os.environ['OPENSHIFT_PYTHON_PORT'],
-    'logs_dir': os.environ['OPENSHIFT_PYTHON_LOG_DIR'],
-    'eggs_dir': data_dir + 'eggs',
-    'dbs_dir': data_dir + 'dbs',
-    'items_dir': data_dir + 'items'
-  }
-
-  scrapyd_conf = "[scrapyd]\n" + \
-    "\n".join('{}={}'.format(key, val) for key, val in config_values.items())
- 
-  with open('scrapyd.conf', 'w') as f:
-    f.write(scrapyd_conf)
-
-  os.system("scrapyd") 
+    data_dir = os.environ['OPENSHIFT_DATA_DIR']
+    try:
+        config_values = {
+            'bind_address': os.environ['OPENSHIFT_PYTHON_IP'],
+            'http_port': os.environ['OPENSHIFT_PYTHON_PORT'],
+            'logs_dir': os.environ['OPENSHIFT_PYTHON_LOG_DIR'],
+            'eggs_dir': data_dir + 'eggs',
+            'dbs_dir': data_dir + 'dbs',
+            'items_dir': data_dir + 'items'
+        }
+    except:
+        
+        config_values = {
+            'bind_address': os.environ['OPENSHIFT_DIY_IP'],
+            'http_port': os.environ['OPENSHIFT_DIY_PORT'],
+            'logs_dir': os.environ['OPENSHIFT_DIY_LOG_DIR'],
+            'eggs_dir': data_dir + 'eggs',
+            'dbs_dir': data_dir + 'dbs',
+            'items_dir': data_dir + 'items'
+        }
+    scrapyd_conf = "[scrapyd]\n" + \
+                   "\n".join('{}={}'.format(key, val) for key, val in config_values.items())
+    with open('scrapyd.conf', 'w') as f:
+        f.write(scrapyd_conf)
+    os.system("scrapyd") 
